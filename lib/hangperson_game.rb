35afe80cef1c778e13e52acaa533b95f10eb9aed
Alain ANDRE <dev@alain-andre.fr>
2017-06-17 21:50:16 +0200

    1er etape

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -5,11 +5,32 @@ class HangpersonGame
 
   # Get a word from remote "random word" service
 
-  # def initialize()
-  # end
+  attr_accessor :word, :guesses, :wrong_guesses
   
   def initialize(word)
     @word = word
+    @guesses = @wrong_guesses = ''
+  end
+
+  def guess(word)
+    raise ArgumentError if word == ""
+    raise ArgumentError if ((word =~ /[\w]/i) == nil) == true
+    if ((word =~ /[#{@word}]/i).class == Fixnum) == true
+      if @guesses.empty?
+        @guesses = word 
+        return true
+      else 
+        if ((word =~ /[#{@guesses}]/i).class == Fixnum) == true # repeated
+          return false
+        else
+          @guesses = word
+          return true
+        end
+      end
+    else
+      @wrong_guesses = word 
+      return true
+    end
   end
 
   def self.get_random_word
