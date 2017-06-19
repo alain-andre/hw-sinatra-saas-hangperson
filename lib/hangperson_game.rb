292863e6f5f46649d9977c45e338f46fadbf2e45
Alain ANDRE <contact@alain-andre.fr>
2017-06-19 16:09:04 +0200

    exercice 2 terminé

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -6,33 +6,63 @@ class HangpersonGame
   # Get a word from remote "random word" service
 
   attr_accessor :word, :guesses, :wrong_guesses
+  attr_reader   :word_with_guesses
   
   def initialize(word)
     @word = word
     @guesses = @wrong_guesses = ''
+    @word_with_guesses = '' << "-" * @word.length
   end
 
   def guess(word)
-    raise ArgumentError if word == ""
+    raise ArgumentError if word == nil
+    raise ArgumentError if word.empty? || word == nil
     raise ArgumentError if ((word =~ /[\w]/i) == nil) == true
-    if ((word =~ /[#{@word}]/i).class == Fixnum) == true
-      if @guesses.empty?
-        @guesses = word 
-        return true
-      else 
-        if ((word =~ /[#{@guesses}]/i).class == Fixnum) == true # repeated
-          return false
-        else
-          @guesses = word
-          return true
-        end
+    # Correct guess
+    if (@word =~ /#{word}/i).class == Fixnum
+      if (@guesses =~ /#{word}/i).class == Fixnum # repeated
+        return false
       end
-    else
-      @wrong_guesses = word 
+      set_word_with_guesses word
+    else # wrong guess
+      if (@wrong_guesses =~ /#{word}/i).class == Fixnum # repeated
+        return false
+      else
+        @wrong_guesses += word
+      end
       return true
     end
   end
 
+  ## 
+  def set_word_with_guesses word 
+    @guesses = word
+    position = 0
+    @word.each_char do |char|
+      if (char =~ /#{word}/i).class == Fixnum
+        @word_with_guesses[position] = word    
+      end
+      position += 1
+    end
+  end
+
+  ## 
+  def check_win_or_lose
+    return :lose if @wrong_guesses.length == 7
+
+    char_count = 0
+    @word_with_guesses.each_char do |letter|
+      char_count += 1 if (@word =~ /#{letter}/i).class == Fixnum
+    end
+
+    if char_count == @word.length
+      return :win
+    else
+      return :play
+    end
+  end
+
+  ##
   def self.get_random_word
     require 'uri'
     require 'net/http'
